cd07f46d27fcf520e96ff92b79f2967b7bc4de2f
Nick Erdmann <n@nirf.de>
2018-07-15 15:45:34 +0200

    add 64 bit integer types on x86

--- include/arch/x86/types.h
+++ include/arch/x86/types.h
@@ -7,6 +7,8 @@ typedef unsigned short uint16_t;
 typedef short int16_t;
 typedef unsigned long uint32_t;
 typedef long int32_t;
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
 
 typedef uint32_t size_t;
 
